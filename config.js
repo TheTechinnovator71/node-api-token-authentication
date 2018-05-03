94967aad662ff3b4e2b36d20b730334ab1a72b7a
James Foreman <techinnovator71@gmail.com>
2018-05-03 14:49:07 -0400

    initial
    initial

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	'secret': 'ilovescotchyscotch',
-	'database': 'mongodb://dbuser:dbuser@ds053305.mlab.com:53305/myfirstmongodb'
+	'database': 'mongodb://xxxxx:xxxxxxr@ds053305.mlab.com:53305/xxxxxxxx'
 
 };
